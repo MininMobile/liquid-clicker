b45bea91c6944fbd78587f688cda1e32dc3a3c42
MininMobile <miningamer@gmail.com>
2018-02-17 23:22:45 +0000

    why

--- assets/game.js
+++ assets/game.js
@@ -2,7 +2,7 @@ const cvg = require("convergejs");
 
 function Start() {
 	const canvas = document.getElementById("canvas");
-	const c = new cvg.Canvas(canvas, "100%", "100%", 60);
+	const c = new cvg.Canvas(canvas, "600px", "450px", 60);
 
 	var maxLevel = 10;
 	var level = 0;
