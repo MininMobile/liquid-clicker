ca98e39e8c68ab47de5d28fdaaf4fb5304ed4b57
MininMobile <miningamer@gmail.com>
2018-02-17 22:50:43 +0000

    Create Canvas Boio

--- assets/game.js
+++ assets/game.js
@@ -1,6 +1,8 @@
+const cvg = require("convergejs");
+
 function Start() {
 	const canvas = document.getElementById("canvas");
-	const c = null; /* what */
+	const c = cvg.Canvas(canvas, "600px", "450px", 60);
 
 	var maxLevel = 10;
 	var level = 0;
