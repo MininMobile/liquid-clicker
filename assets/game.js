f3dd2dc8b75ccb55c2da4ae3f6420ddcbb308c8d
MininMobile <miningamer@gmail.com>
2018-02-18 00:22:12 +0000

    e x p a n d

--- assets/game.js
+++ assets/game.js
@@ -4,7 +4,7 @@ function Start() {
 	const canvas = document.getElementById("canvas");
 	const c = new cvg.Canvas(canvas, "600px", "450px", 60);
 
-	var maxLevel = 10;
+	var maxLevel = 10000; // mL
 	var level = 0;
 
 	c.style([["background", "url('background.jpg')"],
